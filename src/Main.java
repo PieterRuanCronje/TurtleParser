e72ac45ab98a17c1b76d4bb019b2619de6f2732a
Pieter-Ruan Cronjé <25850083@sun.ac.za>
2023-06-24 16:23:08 +0200

    clean up

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
-        TurtleParser parser = new TurtleParser(args[0]);
-        parser.printDataTurtle();
-        //parser.printDataCSV("\t");
-    }
+  public static void main(String[] args) {
+    TurtleParser parser = new TurtleParser(args[0]);
+    parser.printDataTurtle();
+    // parser.printDataCSV("\t");
+  }
 }
